bd1eabd72c4ab9ddd78977bf701c6e77b9f1141d
Mata <Edson.Mata@Honeywell.com>
2022-03-03 12:25:26 -0600

    primer commit

---
+++ votingBot.py
@@ -0,0 +1,16 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+for x in range(3):
+    PATH = "C:\Program Files (x86)\chromedriver.exe"
+    driver = webdriver.Chrome(PATH)
+    driver.get("https://www.selenium.dev/documentation/")
+    search = driver.find_element_by_link_text("WebDriver")
+    action = webdriver.common.action_chains.ActionChains(driver)
+    action.move_to_element_with_offset(search, 5, 5)
+    action.click()
+    action.perform()
+    driver.quit()
+    print(x)
+
